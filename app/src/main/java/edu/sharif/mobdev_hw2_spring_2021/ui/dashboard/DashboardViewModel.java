fecac2b97c27e169d0f380ebbcc2adba176fb6e5
yasharzb <yasharzb@ce.sharif.edu>
2021-04-29 11:04:20 +0430

    Initial commit

---
+++ app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package edu.sharif.mobdev_hw2_spring_2021.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
