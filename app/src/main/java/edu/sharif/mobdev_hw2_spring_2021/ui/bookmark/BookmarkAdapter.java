6db34fdf2c7c376f8898810c8e3b1145a1687ad6
yasharzb <yasharzb@ce.sharif.edu>
2021-05-04 13:42:36 +0430

    Add scrolling and searching

--- app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/bookmark/BookmarkAdapter.java
+++ app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/bookmark/BookmarkAdapter.java
@@ -1,5 +1,7 @@
 package edu.sharif.mobdev_hw2_spring_2021.ui.bookmark;
 
+import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +10,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import edu.sharif.mobdev_hw2_spring_2021.R;
 import edu.sharif.mobdev_hw2_spring_2021.db.dao.BookmarkRepository;
@@ -33,9 +40,14 @@ public class BookmarkAdapter extends RecyclerView.Adapter<BookmarkViewHolder> {
     @NonNull
     private final List<BookmarkDTO> bookmarks;
     private FragmentManager fragmentManager;
+    private RecyclerView recyclerView;
+    private Activity activity;
 
-    public static BookmarkAdapter getInstance(FragmentManager fragmentManager) {
+
+    public static BookmarkAdapter getInstance(RecyclerView recyclerView, Activity activity, FragmentManager fragmentManager) {
         BOOKMARK_ADAPTER.fragmentManager = fragmentManager;
+        BOOKMARK_ADAPTER.recyclerView = recyclerView;
+        BOOKMARK_ADAPTER.activity = activity;
         return BOOKMARK_ADAPTER;
     }
 
@@ -65,6 +77,33 @@ public class BookmarkAdapter extends RecyclerView.Adapter<BookmarkViewHolder> {
     private void deleteBookmarkWithDialog(String bookmarkName) {
         DeleteBookmarkDialog deleteBookmarkDialog = new DeleteBookmarkDialog(this, bookmarkName);
         deleteBookmarkDialog.show(fragmentManager, "BookmarkDeletionDialog");
+    }
+
+    private void setUpScrolling() {
+        AtomicBoolean loading = new AtomicBoolean(true);
+        AtomicInteger pastVisibleItems = new AtomicInteger(0);
+        AtomicInteger visibleItemCount = new AtomicInteger(0);
+        AtomicInteger totalItemCount = new AtomicInteger(0);
+        LinearLayoutManager layoutManager;
+        layoutManager = new LinearLayoutManager(activity);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NotNull RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) {
+                    visibleItemCount.set(layoutManager.getChildCount());
+                    totalItemCount.set(layoutManager.getItemCount());
+                    pastVisibleItems.set(layoutManager.findFirstVisibleItemPosition());
+
+                    if (loading.get()) {
+                        if ((visibleItemCount.get() + pastVisibleItems.get()) >= totalItemCount.get()) {
+                            loading.set(false);
+                            loading.set(true);
+                        }
+                    }
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/bookmark/BookmarkFragment.java
+++ app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/bookmark/BookmarkFragment.java
@@ -1,9 +1,12 @@
 package edu.sharif.mobdev_hw2_spring_2021.ui.bookmark;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,15 +15,20 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import edu.sharif.mobdev_hw2_spring_2021.R;
+import edu.sharif.mobdev_hw2_spring_2021.db.dao.BookmarkRepository;
+import edu.sharif.mobdev_hw2_spring_2021.db.entity.Bookmark;
+import edu.sharif.mobdev_hw2_spring_2021.model.coin.BookmarkDTO;
 
 public class BookmarkFragment extends Fragment {
 
     private BookmarkViewModel bookmarkViewModel;
     private RecyclerView recyclerView;
-    private final String TAG = "TAG-bmf";
+    private static BookmarkAdapter adapter;
 
 
     @Override
@@ -31,13 +39,39 @@ public class BookmarkFragment extends Fragment {
         recyclerView = root.findViewById(R.id.bookmark_list);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
-        recyclerView.setAdapter(BookmarkAdapter.getInstance(getChildFragmentManager()));
+        adapter = BookmarkAdapter.getInstance(recyclerView, getActivity(), getChildFragmentManager());
+        recyclerView.setAdapter(adapter);
         final TextView textView = root.findViewById(R.id.text_bookmark);
+        final EditText editText = root.findViewById(R.id.bookmark_search_bar);
+        setUpSearchListener(editText);
         bookmarkViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        if (Objects.requireNonNull(recyclerView.getAdapter()).getItemCount() != 0)
+        if (adapter.getItemCount() != 0)
             textView.setVisibility(View.INVISIBLE);
         return root;
     }
 
+    private void setUpSearchListener(EditText editText) {
+        List<BookmarkDTO> bookmarkDTOS = new ArrayList<>(adapter.getBookmarks());
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<BookmarkDTO> tempBookmarks = new ArrayList<>();
+                bookmarkDTOS.forEach(bookmarkDTO -> {
+                    if (bookmarkDTO.getName().toLowerCase().contains(s.toString().toLowerCase()))
+                        tempBookmarks.add(bookmarkDTO);
+                });
+                adapter.getBookmarks().clear();
+                adapter.getBookmarks().addAll(tempBookmarks);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
 }
