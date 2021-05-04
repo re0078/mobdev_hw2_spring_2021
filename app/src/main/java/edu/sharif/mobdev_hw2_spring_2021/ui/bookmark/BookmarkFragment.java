ced9650390978ffafbeac5fc020ff5c288e6c2cb
yasharzb <yasharzb@ce.sharif.edu>
2021-05-04 12:13:18 +0430

    Improve layouts

--- app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/bookmark/BookmarkFragment.java
+++ app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/bookmark/BookmarkFragment.java
@@ -1,28 +1,20 @@
 package edu.sharif.mobdev_hw2_spring_2021.ui.bookmark;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
 import java.util.Objects;
 
-import edu.sharif.mobdev_hw2_spring_2021.MainActivity;
 import edu.sharif.mobdev_hw2_spring_2021.R;
-import edu.sharif.mobdev_hw2_spring_2021.model.coin.BookmarkDTO;
-import lombok.Getter;
 
 public class BookmarkFragment extends Fragment {
 
@@ -40,7 +32,7 @@ public class BookmarkFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
         recyclerView.setAdapter(BookmarkAdapter.getInstance(getChildFragmentManager()));
-        final TextView textView = root.findViewById(R.id.text_setting);
+        final TextView textView = root.findViewById(R.id.text_bookmark);
         bookmarkViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         if (Objects.requireNonNull(recyclerView.getAdapter()).getItemCount() != 0)
             textView.setVisibility(View.INVISIBLE);
--- app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/settings/SettingFragment.java
+++ app/src/main/java/edu/sharif/mobdev_hw2_spring_2021/ui/settings/SettingFragment.java
@@ -7,12 +7,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import edu.sharif.mobdev_hw2_spring_2021.MainActivity;
 import edu.sharif.mobdev_hw2_spring_2021.R;
 
 public class SettingFragment extends Fragment {
@@ -27,7 +24,7 @@ public class SettingFragment extends Fragment {
         settingViewModel =
                 new ViewModelProvider(this).get(SettingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_setting, container, false);
-        final TextView textView = root.findViewById(R.id.text_setting);
+        final TextView textView = root.findViewById(R.id.text_bookmark);
         settingViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
